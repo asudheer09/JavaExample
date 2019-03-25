c32499db4bc1ef463eefe10f2594655b4f445531
sudheer <sudheer@sudheer-HP-Notebook>
2019-03-25 20:15:00 +0530

    committing the code changes

---
+++ demo/src/main/java/com/example/demo/controller/MyRestController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MyRestController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+	
+	@RequestMapping("/hi")
+	public List<String> Byehello() {
+		List<String> names=new ArrayList<>();
+		names.add("hello1");
+		names.add("hello2");
+		names.add("hello3");
+		names.add("hello4");
+		names.add("hello5");
+		names.add("hello6");
+
+		return names;
+	}
+
+}
